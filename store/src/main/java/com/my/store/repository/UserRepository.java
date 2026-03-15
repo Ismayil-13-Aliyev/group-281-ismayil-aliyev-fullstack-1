3408dc943a127f708ffa26454cc10443da800ed4
Ismayil-13-Aliyev <aze.ismayil13@gmail.com>
2026-03-15 23:00:48 +0400

    2026 03 08 00 32 39

--- store/src/main/java/com/my/store/repository/UserRepository.java
+++ store/src/main/java/com/my/store/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.my.store.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
